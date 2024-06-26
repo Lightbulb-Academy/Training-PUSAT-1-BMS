11e760c034a728981f7d0b905c40214cd8636fc7
eepos-shashank <shashank@eeposit.com>
2024-06-26 11:24:27 +0545

    WIP reservation CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { BooksModule } from './books/books.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [BooksModule, PrismaModule, UsersModule],
+  imports: [BooksModule, PrismaModule, UsersModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
